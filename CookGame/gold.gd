4da778be578fe9272b12248eeb27f66e47436c5d
unknown <freeutil06@gmail.com>
2025-05-22 02:14:54 +0900

    Food sprite 추가

--- CookGame/gold.gd
+++ CookGame/gold.gd
@@ -1,20 +1,21 @@
 extends Label
 
-var gold := 3000
-var gold_max := 10000
+#var gold := 3000
+#var gold_max := 10000
+var gm = GameManager
 
 func _ready():
 	update_gold_text()
 
 func set_gold(new_gold: int):
-	gold = clamp(new_gold, 0, gold_max)
+	gm.gold = new_gold
 	update_gold_text()
 
 func add_gold(amount: int):
-	set_gold(gold + amount)
+	set_gold(gm.gold + amount)
 
 func subtract_gold(amount: int):
-	set_gold(gold - amount)
+	set_gold(gm.gold - amount)
 
 func update_gold_text():
-	text = " %d / %d" % [gold, gold_max]
+	text = " %d" % [gm.gold]
